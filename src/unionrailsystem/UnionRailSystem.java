fbece3e810da123b4972b60fd1238fcdc1d7e1e5
Thomas Baxendale <tombaxendale95@gmail.com>
2022-08-30 20:00:34 +0100

    updated vs code files

---
+++ src/unionrailsystem/UnionRailSystem.java
@@ -0,0 +1,11 @@
+package unionrailsystem;
+
+import com.fs.starfarer.api.BaseModPlugin;
+
+public class UnionRailSystem extends BaseModPlugin {
+
+    @Override
+    public void onApplicationLoad() throws Exception {
+        throw new Exception("Mod template successfully loaded. Happy modding.");
+    }
+}
