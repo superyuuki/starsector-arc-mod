c0b2fdaebdca133ffba4e693eba255a4d5fa6de7
Thomas Baxendale <tombaxendale95@gmail.com>
2022-09-01 05:44:09 +0100

    add a base range buff to the URS hull mod

--- src/unionrailsystem/URSShipHullMod.java
+++ src/unionrailsystem/URSShipHullMod.java
@@ -1,17 +1,22 @@
 package unionrailsystem;
 
+import java.util.List;
+
 import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableStat;
 import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.WeaponAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class URSShipHullMod extends BaseHullMod {
 
+    private final float WEAPON_RANGE_FLAT_MODIFIER = 100f;
     private final float WEAPON_PERCENT_MODIFIER = 20f;
 
     @Override
     public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
-
         ship.addListener(new URSFactionDamageMod(WEAPON_PERCENT_MODIFIER));
+        ship.addListener(new URSFactionRangeMod(WEAPON_RANGE_FLAT_MODIFIER));
     }
 
     @Override
@@ -21,7 +26,8 @@ public class URSShipHullMod extends BaseHullMod {
 
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
-        if (index == 0) return "" + (int) WEAPON_PERCENT_MODIFIER + "%";   
+        if (index == 0) return "" + (int) WEAPON_RANGE_FLAT_MODIFIER;     
+        if (index == 1) return "" + (int) WEAPON_PERCENT_MODIFIER + "%";
         return null; 
             
         
